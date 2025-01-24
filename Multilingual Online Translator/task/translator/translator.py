d94feb3b408c885407641cb56cfa42654ba1ccdc
renbaum <renbaum04@bluewin.ch>
2025-01-24 14:30:04 +0100

    All of them

--- Multilingual Online Translator/task/translator/translator.py
+++ Multilingual Online Translator/task/translator/translator.py
@@ -1,33 +1,65 @@
 import requests
 from bs4 import BeautifulSoup
 
+languages = ["Arabic",
+    "German",
+    "English",
+    "Spanish",
+    "French",
+    "Hebrew",
+    "Japanese",
+    "Dutch",
+    "Polish",
+    "Portuguese",
+    "Romanian",
+    "Russian",
+    "Turkish",
+]
+
+class Language:
+    def __init__(self):
+        self.sourceLanguage = 0
+        self.targetLanguage = 0
+
+    def setSourceLanguage(self):
+        print("Type the number of your language:")
+        self.sourceLanguage = int(input()) - 1
+
+    def setTargetLanguage(self):
+        print("Type the number of language you want to translate to:")
+        self.targetLanguage = int(input()) - 1
+
+    def output_language(self):
+        for i, language in enumerate(languages):
+            print(f"{i+1}. {language}")
+
+    def get_url(self, word: str):
+        translation = f"{languages[self.sourceLanguage].lower()}-{languages[self.targetLanguage].lower()}"
+        return f"https://context.reverso.net/translation/{translation}/{word}"
+
+    def print_title_word(self):
+        print(f"{languages[self.targetLanguage]} Translations:")
+
+    def print_title_phrase(self):
+        print(f"{languages[self.targetLanguage]} Examples:")
+
 if __name__ == '__main__':
-    print('Type "en" if you want to translate from French into English, or "fr" if you want to translate from English into French:')
-    language = input()
+    l = Language()
+    print('Hello, welcome to the translator. Translator supports:')
+    l.output_language()
+    l.setSourceLanguage()
+    l.setTargetLanguage()
+
     print('Type the word you want to translate:')
     word = input()
-    print(f'You chose "{language}" as a language to translate "{word}".')
 
-    translation = ""
-    output_txt = ""
-    output_phrase = ""
-
-    match language:
-        case "en":
-            translation = "french-english"
-            output_txt = "English Translations:"
-            output_phrase = "English Examples:"
-        case "fr":
-            translation = "english-french"
-            output_txt = "French Translations:"
-            output_phrase = "French Examples:"
-
-    url = f"https://context.reverso.net/translation/{translation}/{word}"
+    url = l.get_url(word)
     headers = {'User-Agent': 'Mozilla/5.0 AppleWebKit/537.36 Chrome/93.0.4577.82 Safari/537.36'}
     user_agent = 'Mozilla/5.0'
     user_agent2 = 'Mozilla/5.0 AppleWebKit/537.36 Chrome/93.0.4577.82 Safari/537.36'
+#    print(url)
     result = requests.get(url, headers={'User-Agent': user_agent2})
-    print(result.status_code)
+#    print(result.status_code)
     if result.status_code == 200:
         soup = BeautifulSoup(result.text, 'html.parser')
 
@@ -39,14 +71,13 @@ if __name__ == '__main__':
         phrase_source_trans = [phrase.get_text().strip() for phrase in phrase_source_tags]
         phrase_target_trans = [phrase.get_text().strip() for phrase in phrase_target_tags]
 
-
-        print(output_txt)
+        l.print_title_word()
         for word in trans_words:
             print(word)
 
         print()
 
-        print(output_phrase)
+        l.print_title_phrase()
         for source, target in zip(phrase_source_trans, phrase_target_trans):
             print(source)
             print(target)
