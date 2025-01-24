3d16962ff58c0e51096307498b4bb55a40a97db9
renbaum <renbaum04@bluewin.ch>
2025-01-24 08:44:33 +0100

    Initial commit

---
+++ Multilingual Online Translator/task/translator/translator.py
@@ -0,0 +1,9 @@
+
+
+
+if __name__ == '__main__':
+    print('Type "en" if you want to translate from French into English, or "fr" if you want to translate from English into French:')
+    language = input()
+    print('Type the word you want to translate:')
+    word = input()
+    print(f'You chose "{language}" as a language to translate "{word}".')
